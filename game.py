c241d98803f704cea1b1fa2fd3b27a4e87012b43
gituser1034 <olly.love@ontariotechu.net>
2025-12-07 22:15:09 -0500

    game near done

--- game.py
+++ game.py
@@ -6,6 +6,10 @@ from PIL import Image, ImageTk
 
 class MyApp(tk.Frame):
     def __init__(self, root):
+
+        self.current_page_index = 0
+        self.pages = [self.create_page_container, self.hint_page, self.game_page1]
+
         super().__init__(
             root
         )
@@ -20,7 +24,12 @@ class MyApp(tk.Frame):
 
     def load_main_widgets(self):
         self.create_page_container()
-        #self.create_pager()
+        self.pages[self.current_page_index]()
+
+    # When switching pages, clear page first
+    def clear_frame(self,frame):
+        for child in frame.winfo_children():
+            child.destroy()
 
     # Container for whole page - set up right amount of columns and rows for titles
     # Authors, rules label, rules text box, and start button, with background image
@@ -28,6 +37,11 @@ class MyApp(tk.Frame):
         self.page_container = tk.Frame(
             self.main_frame
         )
+
+        def change_page():
+            self.clear_frame(self.page_container)
+            self.current_page_index = 1
+            self.pages[self.current_page_index]()
 
         # Background image
         self.bg_image = ImageTk.PhotoImage(Image.open("titanic.jpg"))
@@ -59,6 +73,7 @@ class MyApp(tk.Frame):
             else:
                 self.page_container.rowconfigure(i,weight=0)
 
+        # Makes container take up the whole screen
         self.page_container.grid(column=0, row=0, sticky=tk.NSEW)
 
         # This stuff would be moved to a different function, this not structure but what
@@ -73,13 +88,113 @@ class MyApp(tk.Frame):
         rule_txt = tk.Label(self.page_container, text=rules, font=("Helvetica", 12),fg="white",bg="#217fdd",wraplength=400,anchor="w",justify="left")
         rule_txt.grid(row=3,column=0,sticky="w")
         # Want to add an on click to change scenes
-        start_btn = tk.Button(self.page_container, text="Start", font=("Helvetica", 20), fg="white", bg="#217fdd")
+        start_btn = tk.Button(self.page_container, text="Start", font=("Helvetica", 20), fg="white", bg="#217fdd",command=change_page)
         start_btn.grid(row=4,column=1)
 
-    # Will have new background with some image of a character, textbox with there story
+    # Will have new background (or same image) with some image of a character, textbox with there story
     # survive or not buttons, next button. Each survive/not will run the model, use placeholder for now.
-    def page1():
-        pass
+
+    # This page black with textbox of hints to aid users in geussing.
+    def hint_page(self):
+        self.page_container = tk.Frame(
+            self.main_frame,
+            bg="#000000"
+        )
+
+        def change_page():
+            self.clear_frame(self.page_container)
+            self.current_page_index = 2
+            self.pages[self.current_page_index]()
+
+        self.page_container.columnconfigure(0,weight=1)
+        self.page_container.rowconfigure(0,weight=1)
+        self.page_container.rowconfigure(1,weight=1)
+        self.page_container.grid(column=0, row=0, sticky=tk.NSEW)
+
+        hint = """
+        To aid you in making your choices - women and children were prioritized
+        access to lifeboats. The nicer ($$$) living quarters were closer to the top of the ship.
+        Lifeboats were deployed from the top of the ship.
+        """
+
+        hint_txt = tk.Label(self.page_container,text=hint, font=("Helvetica", 20),wraplength=500,fg="white",bg="#000000",justify="center")
+        hint_txt.grid(row=0, column=0,sticky="s",columnspan=2)
+        continue_btn = tk.Button(self.page_container, text="Continue", font=("Helvetica", 20), fg="white", bg="#000000",command=change_page)
+        continue_btn.grid(row=1,column=1, sticky="s")
+
+    # Finish format for game page, and other pages
+    # Then connect to model to actually run geusses
+    def game_page1(self):
+        self.page_container = tk.Frame(
+            self.main_frame,
+            bg="#217fdd"
+        )
+
+        def change_page():
+            self.clear_frame(self.page_container)
+            self.current_page_index += 1
+            self.pages[self.current_page_index]()
+
+        self.page_container.columnconfigure(0,weight=1)
+        self.page_container.columnconfigure(1,weight=1)
+        self.page_container.columnconfigure(2,weight=1)
+        self.page_container.rowconfigure(0,weight=1)
+        self.page_container.rowconfigure(1,weight=1)
+        self.page_container.rowconfigure(2,weight=1)
+        self.page_container.rowconfigure(3,weight=1)
+        self.page_container.grid(column=0, row=0, sticky=tk.NSEW)
+
+        # Didn't Survive - PassengerId = 1, info from dataset + made up details on top
+        # related to the dataset info
+        # Will store an array in here to and run it through dataset later
+        story = """
+        Story 1:
+        Hi, I'm Mr. Owen Harris Braund. I heard about the Titanic all over the news
+        and just had to go. I worked extra hours as a server to make just enough
+        for a 3rd class ticket. Its hard finding work at my age as I'm only 22, not
+        many people want to hire someone like me, especially with no university education.
+        I worked very hard to be here, I even managed to grab an extra ticket for my brother Lewis. 
+        I see all these families around, I'm so grateful I don't have any kids to look
+        after, that seems like a tough job. Though, some of those families are living on 
+        the upper decks, I'm in a shared cabin at the bottom of the ship.
+        Theres always a compromise, but I'm enjoying my time here anyways.
+        """
+        
+        # Titanic man 1
+        self.portrait = ImageTk.PhotoImage(Image.open("titanicman1.jpg"))
+        image_label = tk.Label(self.page_container, image=self.portrait)
+        # Change to grid
+        # image_label.place(x=0, y=0, relwidth=1, relheight=1)
+        # image_label.lower()
+        image_label.grid(row=0,column=0,rowspan=4)
+        story_txt = tk.Label(self.page_container,text=story, font=("Helvetica", 12),wraplength=500,fg="white",bg="#217fdd",anchor="w")
+        story_txt.grid(row=0, column=1,columnspan=2,sticky="w")
+        question_txt = tk.Label(self.page_container,text="Did they survive the Titanic?", font=("Helvetica", 12),fg="white",bg="#217fdd")
+        question_txt.grid(row=1, column=1,columnspan=2,sticky="n")
+        no_btn = tk.Button(self.page_container, text="No", font=("Helvetica", 20), fg="white", bg="#7A0A0A")
+        no_btn.grid(row=2,column=1,sticky="s")
+        yes_btn = tk.Button(self.page_container, text="Yes", font=("Helvetica", 20), fg="white", bg="#0b711a")
+        yes_btn.grid(row=2,column=2,sticky="s")
+        next_btn = tk.Button(self.page_container, text="Next", font=("Helvetica", 20), fg="white", bg="#217fdd")
+        next_btn.grid(row=3,column=2,sticky="se")
+    
+    def game_page2(self):
+        self.page_container = tk.Frame(
+            self.main_frame,
+            bg="#217fdd"
+        )
+
+        def change_page():
+            self.clear_frame(self.page_container)
+            self.current_page_index += 1
+            self.pages[self.current_page_index]()
+        
+        # Titanic man 1
+        self.portrait = ImageTk.PhotoImage(Image.open("titanicman1.jpg"))
+        image_label = tk.Label(self.page_container, image=self.portrait)
+        # Change to grid
+        image_label.place(x=0, y=0, relwidth=1, relheight=1)
+        image_label.lower()
 
 root = tk.Tk()
 root.title('Titanic Guesser')
