e03897dba00e18943cb784b9ab6a6cf3f5ea78c3
Nathan Singer <nathan.r.singer@gmx.com>
2025-12-08 14:29:06 -0500

    fix for non-gpu havers

--- deployment_game.py
+++ deployment_game.py
@@ -47,8 +47,12 @@ class MyApp(tk.Frame):
     # Evaluates the features of the titanic passengers story
     # returning 1 or 0 for whether they survived or not
     def ml_evaluate(self):
-        #model: MLPClassifier = torch.load("models/mlp_onelayer_model.mdl", weights_only=False)
-        model: MLP4DResidualClassifier = torch.load("models/mlp_fourlayer_model.mdl", weights_only = False)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if device != "cpu":
+            model: MLP4DResidualClassifier = torch.load("models/mlp_fourlayer_model.mdl", weights_only = False)
+        else:
+            model: MLP4DResidualClassifier = torch.load("models/mlp_fourlayer_model.mdl", weights_only = False, map_location=device)
+
         model.eval()
         x = torch.tensor([self.current_features], dtype=torch.float32)
         with torch.no_grad():
